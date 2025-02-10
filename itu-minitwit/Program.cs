f467cee9e6761f15c5efbe1c7bae79bfce543644
JacquesPuvis <japu@itu.dk>
2025-02-10 11:48:08 +0100

    Configure middleware in program.cs
    Added middleware to support razor pages
    
    Co-Authored-By: Chat-gpt <>
    Co-Authored-By: RasmusLarsen02 <105587694+RasmusLarsen02@users.noreply.github.com>
    Co-Authored-By: anthoncastillo <145575420+anthoncastillo@users.noreply.github.com>
    Co-Authored-By: Mathias <62756914+math045b@users.noreply.github.com>
    Co-Authored-By: Christoffer grünberg <70943516+stoftot@users.noreply.github.com>

--- itu-minitwit/Program.cs
+++ itu-minitwit/Program.cs
@@ -12,19 +12,37 @@ using Microsoft.EntityFrameworkCore;
 using itu_minitwit.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // Set session timeout duration
+    options.Cookie.HttpOnly = true; // Make session cookie HttpOnly
+    options.Cookie.IsEssential = true; // Mark session cookie as essential
+});
 builder.Services.AddDbContext<MiniTwitDbContext>(options =>
     options.UseSqlite("Data Source=minitwit.db"));
 
 builder.Services.AddRazorPages();
 
+<<<<<<< Updated upstream
 var app = builder.Build();  
+=======
+var app = builder.Build();
+>>>>>>> Stashed changes
 
+app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseSession();
 app.UseRouting();
 
+app.UseAuthentication();
+app.UseAuthorization();
 
 
 
+app.MapRazorPages();
 
 
 app.Run();
