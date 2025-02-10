3b0a08604e64c93b7fef8794f139a8555cf72f16
Christoffer grünberg <gryn@itu.dk>
2025-02-10 12:50:05 +0100

    Updated register model

--- itu-minitwit/Pages/Register.cshtml.cs
+++ itu-minitwit/Pages/Register.cshtml.cs
@@ -1,7 +1,9 @@
+using itu_minitwit.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-public class RegisterModel : PageModel
+public class RegisterModel(MiniTwitDbContext db, IPasswordHasher<string> passwordHasher) : PageModel
 {
     [BindProperty] public string Username { get; set; }
     [BindProperty] public string Email { get; set; }
@@ -17,6 +19,16 @@ public class RegisterModel : PageModel
             return;
         }
 
+        User user = new User
+        {
+            Username = Username,
+            Email = Email,
+            PwHash = passwordHasher.HashPassword(Username, Password)
+        };
+        
+        db.Users.Add(user);
+        db.SaveChanges();
+        
         TempData["FlashMessages"] = new List<string> { "Registration successful!" };
         Response.Redirect("/Login");
     }
