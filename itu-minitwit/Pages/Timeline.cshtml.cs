b863cca16f47f18dc9db9ac3d2d2d1a55a5333a7
stoftot <gryn@itu.dk>
2025-02-10 17:47:52 +0100

    Added the abillity to write messages

--- itu-minitwit/Pages/Timeline.cshtml.cs
+++ itu-minitwit/Pages/Timeline.cshtml.cs
@@ -9,6 +9,7 @@ namespace itu_minitwit.Pages;
 
 public class TimelineModel(MiniTwitDbContext db) : PageModel
 {
+    [BindProperty] public string Message { get; set; }
     public string PageTitle { get; set; } = "My Timeline";
     public bool IsUserLoggedIn => HttpContext.Session.GetString("User") != null;
     public string Username => HttpContext.Session.GetString("User") ?? "Guest";
@@ -92,18 +93,32 @@ public class TimelineModel(MiniTwitDbContext db) : PageModel
             })
             .ToList();
     }
+    
     private string GetGravatarUrl(string email, int size = 50)
     {
         var hash = MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(email.Trim().ToLower()));
         var hashString = string.Concat(hash.Select(b => b.ToString("x2")));
         return $"https://www.gravatar.com/avatar/{hashString}?d=identicon&s={size}";
     }
-    public IActionResult OnPost(string text)
+    
+    public IActionResult OnPost()
     {
-        if (!string.IsNullOrWhiteSpace(text))
+        if (string.IsNullOrWhiteSpace(Message))
         {
-            TempData["FlashMessages"] = new List<string> { "Message posted successfully!" };
+               
         }
+
+        Data.Message message = new Message
+        {
+            AuthorId = db.Users.FirstOrDefault(c => c.Username == Username).UserId,
+            Flagged = 0,
+            PubDate = (int)DateTimeOffset.Now.ToUnixTimeSeconds(),
+            Text = Message,
+        };
+
+        db.Messages.Add(message);
+        db.SaveChanges();
+        
         return RedirectToPage();
     }
 }
