410b8dc4c3775d4014b010f8f4f6b33183e763f9
stoftot <gryn@itu.dk>
2025-02-14 16:44:33 +0100

    Create LatestService.cs

---
+++ itu-minitwit/minitwit.web/LatestService.cs
@@ -0,0 +1,33 @@
+﻿namespace itu_minitwit;
+
+public class LatestService
+{
+    private const string FilePath = "./latest_processed_sim_action_id.txt";
+
+    public async Task<int> GetLatest()
+    {
+        var latestProcessedCommandId = -1;
+
+        if (!File.Exists(FilePath)) return -1;
+        
+        try
+        {
+            var content = await File.ReadAllTextAsync(FilePath);
+            latestProcessedCommandId = int.Parse(content);
+        }
+        catch
+        {
+            latestProcessedCommandId = -1;
+        }
+
+        return latestProcessedCommandId;
+    }
+
+    public async Task UpdateLatest(int latest = -1)
+    {
+        if (latest != -1)
+        { 
+            await File.WriteAllTextAsync(FilePath, latest.ToString());
+        }
+    }
+}
