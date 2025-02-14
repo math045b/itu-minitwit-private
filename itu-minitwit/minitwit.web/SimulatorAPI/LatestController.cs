d6c446174b3ad7fe18ed3b7e53ffdbe1027727ae
stoftot <gryn@itu.dk>
2025-02-14 16:45:01 +0100

    Create LatestController.cs

---
+++ itu-minitwit/minitwit.web/SimulatorAPI/LatestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace itu_minitwit.SimulatorAPI;
+
+[Route("api[Controller]")]
+[ApiController]
+public class LatestController(LatestService latestService) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult> GetLatest()
+    {
+        var latestProcessedCommandId = await latestService.GetLatest();
+
+        return Ok(new { latest = latestProcessedCommandId });
+    }
+}
