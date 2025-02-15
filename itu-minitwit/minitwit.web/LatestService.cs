13f2dcf3c29f8ef8e900b574508f70bbb5536c38
stoftot <gryn@itu.dk>
2025-02-15 09:37:47 +0100

    Updated latest service to use, the databse instead of a file

--- itu-minitwit/minitwit.web/LatestService.cs
+++ itu-minitwit/minitwit.web/LatestService.cs
@@ -1,33 +1,27 @@
-﻿namespace itu_minitwit;
+﻿using itu_minitwit.Data;
+using Microsoft.EntityFrameworkCore;
 
-public class LatestService
+namespace itu_minitwit;
+
+public class LatestService(MiniTwitDbContext dbContext)
 {
-    private const string FilePath = "./latest_processed_sim_action_id.txt";
-
     public async Task<int> GetLatest()
     {
-        var latestProcessedCommandId = -1;
-
-        if (!File.Exists(FilePath)) return -1;
-        
-        try
-        {
-            var content = await File.ReadAllTextAsync(FilePath);
-            latestProcessedCommandId = int.Parse(content);
-        }
-        catch
-        {
-            latestProcessedCommandId = -1;
-        }
-
-        return latestProcessedCommandId;
+        var latestProcessedCommandId = await dbContext.LatestProcessedSimActions.FirstOrDefaultAsync();
+        if (latestProcessedCommandId == null) return -1;
+        return latestProcessedCommandId.Id;
     }
 
     public async Task UpdateLatest(int latest)
     {
         if (latest != -1)
-        { 
-            await File.WriteAllTextAsync(FilePath, latest.ToString());
+        {
+            await dbContext.LatestProcessedSimActions.ExecuteDeleteAsync();
+
+            var latestObj = new LatestProcessedSimAction { Id = latest };
+
+            await dbContext.LatestProcessedSimActions.AddAsync(latestObj);
+            await dbContext.SaveChangesAsync();
         }
     }
 }
