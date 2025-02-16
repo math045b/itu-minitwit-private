f29c361e92e35f2351dbbf96fb97436fc286477a
Mathias <math045b@gmail.com>
2025-02-16 22:49:43 +0100

    Created Register endpoint
    Not finished yet

--- itu-minitwit/minitwit.web/SimulatorAPI/RegisterController.cs
+++ itu-minitwit/minitwit.web/SimulatorAPI/RegisterController.cs
@@ -1,4 +1,6 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace itu_minitwit.SimulatorAPI;
 
@@ -6,4 +8,36 @@ namespace itu_minitwit.SimulatorAPI;
 [ApiController]
 public class RegisterController(LatestService latestService) : ControllerBase
 {
+    [HttpPost]
+    public async Task<ActionResult> Register([FromForm] string? username, [FromForm] string? email, [FromForm] string? psw)
+    {
+        await latestService.UpdateLatest(-1);
+
+        
+        if (string.IsNullOrWhiteSpace(username))
+        {
+            return new JsonResult(new { status = 400, error_msg = "You have to enter a username"})
+            {
+                StatusCode = 400
+            };
+        }
+
+        if (string.IsNullOrWhiteSpace(email))
+        {
+            return new JsonResult(new { status = 400, error_msg = "You have to enter a valid email address"})
+            {
+                StatusCode = 400
+            };
+        }
+
+        if (string.IsNullOrWhiteSpace(psw))
+        {
+            return new JsonResult(new { status = 400, error_msg = "You have to enter a password" })
+            {
+                StatusCode = 400
+            };
+        }
+
+        return Ok("");
+    }
 }
