5edb59109a3558e29ff16f393503c0d721622e83
anthoncastillo <145575420+anthoncastillo@users.noreply.github.com>
2025-02-17 12:33:01 +0100

    Update FollowerController.cs
    Co-Authored-By: Mathias <62756914+math045b@users.noreply.github.com>

--- itu-minitwit/minitwit.web/SimulatorAPI/FollowerController.cs
+++ itu-minitwit/minitwit.web/SimulatorAPI/FollowerController.cs
@@ -1,6 +1,31 @@
+using System.Net;
+using itu_minitwit.Data;
+using Microsoft.AspNetCore.Mvc;
+
 namespace itu_minitwit.SimulatorAPI;
 
-public class FollowerController
+
+[Route("[Controller]")]
+[ApiController]
+public class FollowerController(MiniTwitDbContext dbContext, LatestService latestService) : ControllerBase
 {
-    
+    [HttpPost("/fllws/{username}")]
+    public async Task<ActionResult> Follow(string username, [FromForm] string? follow)
+    {
+        await latestService.UpdateLatest(-1); //TODO: should change the update number
+        
+        var user = dbContext.Users.FirstOrDefault(u => u.Username == username);
+        var userToFollow = dbContext.Users.FirstOrDefault(u => u.Username == follow);
+
+        if (user == null || userToFollow == null)
+        {
+            return NotFound();
+        }
+        
+        
+        
+        //dbContext.Followers.Add();
+
+        return Ok();
+    }
 }
