c2143b626c6fb67463b9f451392f52cffaadcef3
stoftot <gryn@itu.dk>
2025-02-23 23:07:45 +0100

    Implemented latest repository

---
+++ itu-minitwit/Api/Api.DataAccess/Repositories/LatestRepository.cs
@@ -0,0 +1,25 @@
+﻿using Api.DataAccess.Models;
+using Api.Services.RepositoryInterfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.DataAccess.Repositories;
+
+public class LatestRepository(MinitwitDbContext dbContext) : ILatestRepository
+{
+    public async Task<int> GetLatest()
+    {
+        var latestProcessedCommandId = await dbContext.LatestProcessedSimActions.FirstOrDefaultAsync();
+        if (latestProcessedCommandId == null) return -1;
+        return latestProcessedCommandId.Id;
+    }
+
+    public async Task UpdateLatest(int latest)
+    {
+        await dbContext.LatestProcessedSimActions.ExecuteDeleteAsync();
+
+        var latestObj = new LatestProcessedSimAction { Id = latest };
+
+        await dbContext.LatestProcessedSimActions.AddAsync(latestObj);
+        await dbContext.SaveChangesAsync();
+    }
+}
