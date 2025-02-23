90ebfca8302439aa84ec8473509196dcb765bb90
stoftot <gryn@itu.dk>
2025-02-23 23:07:32 +0100

    Created lateste service interface

---
+++ itu-minitwit/Api/Api.Services/Services/LatestService.cs
@@ -0,0 +1,9 @@
+﻿using Api.Services.RepositoryInterfaces;
+
+namespace Api.Services.Services;
+
+public interface ILatestService
+{
+    public Task<int> GetLatest();
+    public Task UpdateLatest(int? latest);
+}
