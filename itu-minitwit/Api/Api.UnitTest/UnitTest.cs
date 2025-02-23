847161688c9a3deaa27350af13fe93fcb0cbdb23
stoftot <gryn@itu.dk>
2025-02-23 23:18:13 +0100

    Extracted tests

--- itu-minitwit/Api/Api.UnitTest/UnitTest.cs
+++ itu-minitwit/Api/Api.UnitTest/UnitTest.cs
@@ -1,3 +1,7 @@
+using System.Net;
+using System.Text.Json;
+using Api.DataAccess.Models;
+using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace Api.UnitTest;
@@ -9,4 +13,34 @@ public class UnitTest(InMemoryWebApplicationFactory fixture) : IClassFixture<InM
     private readonly HttpClient client = fixture.CreateClient(new WebApplicationFactoryClientOptions
         { AllowAutoRedirect = true, HandleCookies = true });
 
+    [Fact]
+    public async Task GetLatest_FileIsEmpty_Minius1()
+    {
+        fixture.ResetDB();
+        var response = await client.GetAsync("/Latest");
+        var json = await response.Content.ReadAsStringAsync();
+        using var doc = JsonDocument.Parse(json);
+        var latestValue = doc.RootElement.GetProperty("latest").GetInt32();
+        
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        Assert.Equal(-1, latestValue);
+    }
+
+    [Fact]
+    public async Task GetLatest_ThereIsAValue_TheValue()
+    {
+        var dbContext = fixture.GetDbContext();
+        var lastAction = new LatestProcessedSimAction { Id = 230 };
+        await dbContext.AddAsync(lastAction);
+        await dbContext.SaveChangesAsync();
+
+        var response = await client.GetAsync("/Latest");
+        var json = await response.Content.ReadAsStringAsync();
+        using var doc = JsonDocument.Parse(json);
+        var latestValue = doc.RootElement.GetProperty("latest").GetInt32();
+
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        Assert.Equal(lastAction.Id, latestValue);
+    }
 }
