fd107cbfedc543c3aee44d85d7474a39e2c807eb
stoftot <gryn@itu.dk>
2025-02-23 23:08:04 +0100

    Implemented latest service

--- itu-minitwit/Api/Api.Services/Services/LatestService.cs
+++ itu-minitwit/Api/Api.Services/Services/LatestService.cs
@@ -7,3 +7,20 @@ public interface ILatestService
     public Task<int> GetLatest();
     public Task UpdateLatest(int? latest);
 }
+
+public class LatestService(ILatestRepository latestRepository) : ILatestService
+{
+    public Task<int> GetLatest()
+    {
+        return latestRepository.GetLatest();
+    }
+
+    public async Task UpdateLatest(int? latest)
+    {
+        var newLatest = latest ?? -1; 
+        if (newLatest != -1)
+        {
+            await latestRepository.UpdateLatest(newLatest);
+        }
+    }
+}
