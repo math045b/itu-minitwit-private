5d2444b441d8ac0ba44f390fd7a180095ee7cbb3
Mathias <math045b@gmail.com>
2025-02-27 19:58:01 +0100

    Update UserRepository.cs

--- itu-minitwit/Api/Api.DataAccess/Repositories/UserRepository.cs
+++ itu-minitwit/Api/Api.DataAccess/Repositories/UserRepository.cs
@@ -1,6 +1,32 @@
+using Api.CustomExceptions;
+using Api.DataAccess.Models;
+using Api.Services.Dto_s;
+using Api.Services.RepositoryInterfaces;
+using Microsoft.AspNetCore.Identity;
+
 namespace Api.DataAccess.Repositories;
 
-public class UserRepository
+public class UserRepository(MinitwitDbContext db, IPasswordHasher<User> passwordHasher) : IUserRepository
 {
-    
+    public async Task<ReadUserDTO> Register(CreateUserDTO createUserDto)
+    {
+        User user = new User
+        {
+            Username = createUserDto.Username,
+            Email = createUserDto.Email,
+        };
+        user.PwHash = passwordHasher.HashPassword(user, createUserDto.Password);
+
+        try
+        {
+            var createdUser = db.Users.Add(user);
+            await db.SaveChangesAsync();
+            return new ReadUserDTO()
+                { UserId = createdUser.Entity.UserId, Username = user.Username, Email = user.Email };
+        }
+        catch (Microsoft.EntityFrameworkCore.DbUpdateException)
+        {
+            throw new UserAlreadyExists();
+        }
+    }
 }
