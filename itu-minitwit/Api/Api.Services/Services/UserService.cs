9157e4b79cc0deb9f9726aa0c6997125787a206b
Mathias <math045b@gmail.com>
2025-02-27 19:58:09 +0100

    Create UserService.cs

---
+++ itu-minitwit/Api/Api.Services/Services/UserService.cs
@@ -0,0 +1,17 @@
+﻿using Api.Services.Dto_s;
+using Api.Services.RepositoryInterfaces;
+
+namespace Api.Services.Services;
+
+public interface IUserService
+{
+    public ReadUserDTO Register(CreateUserDTO createUserDto);
+}
+
+public class UserService(IUserRepository userRepository) : IUserService
+{
+    public ReadUserDTO Register(CreateUserDTO createUserDto)
+    {
+        return userRepository.Register(createUserDto).Result;
+    }
+}
