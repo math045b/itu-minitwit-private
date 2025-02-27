4816878ac731fadc17c200fc2f2a969cbd51badd
Anthon Castillo Hertzum <anthon.hertzum@gmail.com>
2025-02-27 21:47:19 +0100

    Added custom exceptions for error handling
    Co-Authored-By: Christoffer grünberg <70943516+stoftot@users.noreply.github.com>

---
+++ itu-minitwit/Api/Api.Services/Exceptions/UserDoesntExistException.cs
@@ -0,0 +1,19 @@
+namespace Api.Services.Exceptions;
+
+public class UserDoesntExistException : Exception
+{
+    public UserDoesntExistException()
+    {
+        
+    }
+
+    public UserDoesntExistException(string message)
+    {
+        
+    }
+
+    public UserDoesntExistException(string message, Exception inner)
+    {
+        
+    }
+}
