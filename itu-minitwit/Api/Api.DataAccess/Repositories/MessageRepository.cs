9ac5e3a63bcc6c35ac1a4022925ddc49e64b8421
RasmusLarsen02 <rasmus.rosenmejer@gmail.com>
2025-02-28 13:30:41 +0100

    Implemented Interface with methods

--- itu-minitwit/Api/Api.DataAccess/Repositories/MessageRepository.cs
+++ itu-minitwit/Api/Api.DataAccess/Repositories/MessageRepository.cs
@@ -1,22 +1,66 @@
+using Api.DataAccess.Models;
 using Api.Services.Dto_s.MessageDTO_s;
 using Api.Services.RepositoryInterfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api.DataAccess.Repositories;
 
-public class MessageRepository : IMessageRepository
+public class MessageRepository(MinitwitDbContext dbContext) : IMessageRepository
 {
-    public Task<List<ReadMessageDTO>> ReadMessages()
+    public Task<List<DisplayMessageDTO>> ReadMessages()
     {
-        throw new NotImplementedException();
+        return dbContext.Messages 
+            .Join(dbContext.Users,
+                m => m.AuthorId,
+                a => a.UserId,
+                (m,a) => new DisplayMessageDTO {Username = a.Username, Text = m.Text, PubDate = m.PubDate}
+            )
+            .Take(100)
+            .ToListAsync();
     }
 
-    public Task<List<ReadMessageDTO>> ReadFilteredMessages(string username)
+    public Task<List<DisplayMessageDTO>> ReadFilteredMessages(string username, int pagesize = 100)
     {
-        throw new NotImplementedException();
+        var pageSize = pagesize;
+        var user = dbContext.Users.FirstOrDefault(u => u.Username == username);
+        if (user == null)
+        {
+            throw new KeyNotFoundException("User not found");
+        }
+        
+        return dbContext.Messages
+            .Where(m => m.AuthorId == user.UserId && m.Flagged == 0)
+            .OrderByDescending(m => m.PubDate)
+            .Take(pageSize)
+            .Select(m => new DisplayMessageDTO
+            {
+                Username = username,
+                Text = m.Text,
+                PubDate = m.PubDate,
+            })
+            .ToListAsync();
+        
     }
 
-    public Task<ReadMessageDTO> PostMessage(string username, string content)
+    public Task<bool> PostMessage(string username, string content)
     {
-        throw new NotImplementedException();
+        var user = dbContext.Users.FirstOrDefault(u => u.Username == username);
+        
+        if (user == null)
+        {
+            throw new KeyNotFoundException("User not found");
+        }
+        
+        var message = new Message
+        {
+            AuthorId = user.UserId,
+            Text = content,
+            Flagged = 0,
+            PubDate = (int)DateTimeOffset.Now.ToUnixTimeSeconds(),
+        };
+        
+        dbContext.Messages.Add(message);
+        dbContext.SaveChangesAsync();
+        return Task.FromResult(true);
     }
 }
