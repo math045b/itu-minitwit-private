adb7e9963be3e01d296a17b19a0def9ed445a759
RasmusLarsen02 <rasmus.rosenmejer@gmail.com>
2025-02-28 12:44:13 +0100

    Implemented ReadMessageDTO

--- itu-minitwit/Api/Api.Services/Dto's/MessageDTO's/ReadMessageDTO.cs
+++ itu-minitwit/Api/Api.Services/Dto's/MessageDTO's/ReadMessageDTO.cs
@@ -1,6 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Api.Services.Dto_s.MessageDTO_s;
 
 public class ReadMessageDTO
 {
-    
+    public int MessageId { get; set; }
+    public int UserId { get; set; }
+    public string Content { get; set; }
+    public int? PubDate { get; set; }
+    public int? Flagged { get; set; }
 }
