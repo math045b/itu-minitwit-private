58e15666ad7c78e00d2811e178434a777f9e2aee
stoftot <gryn@itu.dk>
2025-03-01 16:53:46 +0100

    Added follow buttons

---
+++ itu-minitwit/Web/Web/Components/Message.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using Web.Services;
+using Web.Services.DTO_s;
+
+namespace Web.Components;
+
+public class MessageBase : ComponentBase
+{
+    protected DisplayMessageDto MessageDto { get; set; } = new DisplayMessageDto(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras cursus justo ante, et sollicitudin arcu mollis sit amet. Sed luctus tempor nisi et dignissim. Etia",
+        "Mr. Test", "test@test.com", DateTime.Now);
+
+    [Inject] protected UserState userstate { get; set; }
+
+    [Inject] private NavigationManager Navigation { get; set; }
+
+    protected bool DoseLoggedInUserFollowUser()
+    {
+        //TODO: implement
+        return new Random().Next(2) % 2 == 0;
+    }
+
+    protected void Follow()
+    {
+        //TODO: implement
+        Navigation.NavigateTo(Navigation.Uri, forceLoad: true);
+    }
+
+    protected void Unfollow()
+    {
+        //TODO: implement
+        Navigation.NavigateTo(Navigation.Uri, forceLoad: true);
+    }
+}
