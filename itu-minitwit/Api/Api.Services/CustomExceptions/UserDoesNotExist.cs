e6dc5e574fead27cd073c0a1deecb784af4d5f81
Christoffer grünberg <gryn@itu.dk>
2025-03-07 14:22:01 +0100

    Moved and fixed costume exception

--- itu-minitwit/Api/Api.Services/CustomExceptions/UserDoesNotExist.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Api.CustomExceptions;
-
-/// <summary>
-/// Exception to be thrown if some User does not exist in the database
-/// </summary>
-public class UserDoesNotExist() : Exception("User does not exist")
-{
-}
