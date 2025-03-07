22cbc52be4db85414cb4868289526c1b97804ea1
Christoffer grünberg <gryn@itu.dk>
2025-03-07 13:15:49 +0100

    Added non async return decorator

--- itu-minitwit/Api/Api.Services/LogDecorator.cs
+++ itu-minitwit/Api/Api.Services/LogDecorator.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Diagnostics;
+using System.Reflection;
 using ArxOne.MrAdvice.Advice;
 using Serilog;
 using Serilog.Context;
@@ -6,7 +7,7 @@ using Serilog.Context;
 namespace Api.Services;
 
 [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, Inherited = true)]
-public class LogReturnValueAttribute : Attribute, IMethodAsyncAdvice
+public class LogReturnValueAsyncAttribute : Attribute, IMethodAsyncAdvice
 {
     public async Task Advise(MethodAsyncAdviceContext context)
     {
@@ -16,40 +17,53 @@ public class LogReturnValueAttribute : Attribute, IMethodAsyncAdvice
             await context.ProceedAsync();
             
             var returnValue = context.ReturnValue;
-            Log.Information("Method \"{MethodName}\" returned {@ReturnValue}",
+            Log.Information("Method \"{MethodName}\" returned: {@ReturnValue}",
                 context.TargetMethod.Name, returnValue);
 
         }
     }
 }
 
-[AttributeUsage(AttributeTargets.Method, Inherited = true)]
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, Inherited = true)]
+public class LogReturnValueAttribute : Attribute, IMethodAdvice
+{
+    public void Advise(MethodAdviceContext context)
+    {
+        var sourceContext = context.TargetMethod.DeclaringType?.FullName ?? "UnknownSource";
+        using (LogContext.PushProperty("SourceContext", sourceContext))
+        {
+            context.Proceed(); // Proceed with the original method execution
+
+            var returnValue = context.ReturnValue;
+            Log.Information("Method \"{MethodName}\" returned: {@ReturnValue}",
+                context.TargetMethod.Name, returnValue);
+        }
+    }
+}
+
+[AttributeUsage(AttributeTargets.Method)]
 public class LogMethodParametersAttribute : Attribute, IMethodAdvice
 {
     public void Advise(MethodAdviceContext context)
     {
-        // Retrieve method information
-        var method = context.TargetMethod as MethodInfo;
-        if (method == null)
-        {
-            context.Proceed();
-            return;
-        }
-
-        // Get parameter names and values
-        var parameters = method.GetParameters();
-        var arguments = context.Arguments;
-
-        var parameterInfo = parameters
-            .Select((p, i) => $"{p.Name} = {arguments[i] ?? "null"}")
+        
+        // Get the current method info
+        MethodBase method = context.TargetMethod;
+        var methodName = $"{method.Name}";
+        var methodClass = method.DeclaringType?.FullName; 
+        
+        // Get method arguments
+        var args = method.GetParameters()
+            .Select((p, i) => $"{p.Name}: {context.Arguments[i]}")
             .ToArray();
-
+        string argsString = string.Join(", ", args);
+        
         var sourceContext = context.TargetMethod.DeclaringType?.FullName ?? "UnknownSource";
         using (LogContext.PushProperty("SourceContext", sourceContext))
         {
-            Log.Information("Method \"{MethodName}\" called with {@parameterInfo}",
-                context.TargetMethod.Name, parameterInfo);
-
+            Log.Information("Method Called: {MethodName}({Arguments}) in {MethodClass}", 
+                methodName, argsString, methodClass);
+        
         }
         
         // Proceed with the original method execution
