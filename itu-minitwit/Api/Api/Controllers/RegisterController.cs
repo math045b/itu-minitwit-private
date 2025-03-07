9e021638a0df8ff13cd82543b5b0e2ca39cc8ac0
Christoffer grünberg <gryn@itu.dk>
2025-03-07 15:33:26 +0100

    Added logging to register controller

--- itu-minitwit/Api/Api/Controllers/RegisterController.cs
+++ itu-minitwit/Api/Api/Controllers/RegisterController.cs
@@ -1,4 +1,5 @@
 ﻿using Api.CustomExceptions;
+using Api.Services;
 using Api.Services.Dto_s;
 using Api.Services.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -7,58 +8,66 @@ namespace Api.Controllers;
 
 [Route("[Controller]")]
 [ApiController]
-public class RegisterController(IUserService userService, ILatestService latestService)
+public class RegisterController(IUserService userService, ILatestService latestService, ILogger<RegisterController> logger)
     : ControllerBase
 {
+    [LogMethodParameters]
+    [LogReturnValueAsync]
     [HttpPost]
     public async Task<ActionResult> Register([FromForm] string? username, [FromForm] string? email,
         [FromForm] string? psw, [FromQuery] int? latest)
     {
-        await latestService.UpdateLatest(latest);
-
-        if (string.IsNullOrWhiteSpace(username))
-        {
-            return new JsonResult(new { status = 400, error_msg = "You have to enter a username" })
-            {
-                StatusCode = 400
-            };
-        }
-
-        if (string.IsNullOrWhiteSpace(email) || !email.Contains('@'))
-        {
-            return new JsonResult(new { status = 400, error_msg = "You have to enter a valid email address" })
-            {
-                StatusCode = 400
-            };
-        }
-
-        if (string.IsNullOrWhiteSpace(psw))
-        {
-            return new JsonResult(new { status = 400, error_msg = "You have to enter a password" })
-            {
-                StatusCode = 400
-            };
-        }
-
         try
         {
-            userService.Register(new CreateUserDTO() { Username = username, Email = email, Password = psw });
-        }
-        catch (Exception e)
-        {
-            if (e.InnerException is UserAlreadyExists)
+            logger.LogInformation($"Updating latest: {latest?.ToString() ?? "null"}");
+            await latestService.UpdateLatest(latest);
+
+            if (string.IsNullOrWhiteSpace(username))
             {
+                logger.LogError($"Invalid username: {username}");
+                return new JsonResult(new { status = 400, error_msg = "You have to enter a username" })
+                {
+                    StatusCode = 400
+                };
+            }
+
+            if (string.IsNullOrWhiteSpace(email) || !email.Contains('@'))
+            {
+                logger.LogError($"Invalid email: {email}");
+                return new JsonResult(new { status = 400, error_msg = "You have to enter a valid email address" })
+                {
+                    StatusCode = 400
+                };
+            }
+
+            if (string.IsNullOrWhiteSpace(psw))
+            {
+                logger.LogError($"Invalid password {psw}");
+                return new JsonResult(new { status = 400, error_msg = "You have to enter a password" })
+                {
+                    StatusCode = 400
+                };
+            }
+
+            try
+            {
+               await userService.Register(new CreateUserDTO() { Username = username, Email = email, Password = psw });
+            }
+            catch (UserAlreadyExists e)
+            {
+                logger.LogError(e, $"User {username} is already registered");
                 return new JsonResult(new { status = 400, error_msg = "The username is already taken" })
                 {
                     StatusCode = 400
                 };
             }
-            return new JsonResult(new { status = 400, error_msg = e.Message })
-            {
-                StatusCode = 400
-            };
-        }
 
-        return Ok("");
+            return Ok("");
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "An error occured, that we did have not accounted for");
+            return StatusCode(500, "An error occured, that we did not for see");
+        }
     }
 }
