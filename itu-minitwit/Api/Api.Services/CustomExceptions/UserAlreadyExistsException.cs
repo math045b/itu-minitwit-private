e9698672c42f14f64fb66b9ba7bf005a39541201
Christoffer grünberg <gryn@itu.dk>
2025-03-07 15:37:28 +0100

    Update UserAlreadyExistsException.cs

--- itu-minitwit/Api/Api.Services/CustomExceptions/UserAlreadyExistsException.cs
+++ itu-minitwit/Api/Api.Services/CustomExceptions/UserAlreadyExistsException.cs
@@ -3,6 +3,20 @@
 /// <summary>
 /// Exception to be thrown if some User already exist in the database
 /// </summary>
-public class UserAlreadyExists() : Exception("User already exists")
+public class UserAlreadyExists : Exception
 {
+    public UserAlreadyExists()
+    {
+        
+    }
+
+    public UserAlreadyExists(string message)
+    {
+        
+    }
+
+    public UserAlreadyExists(string message, Exception inner)
+    {
+        
+    }
 }
