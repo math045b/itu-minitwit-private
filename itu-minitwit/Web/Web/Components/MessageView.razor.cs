c24e60623f24afb4b811e7e6fd0684995d89f7ed
anthoncastillo <145575420+anthoncastillo@users.noreply.github.com>
2025-03-10 21:53:15 +0100

    deleted line 1 in MessageView.razor and outcommented OnInitializedAsync() in MessageView.razor.cs

--- itu-minitwit/Web/Web/Components/MessageView.razor.cs
+++ itu-minitwit/Web/Web/Components/MessageView.razor.cs
@@ -11,16 +11,16 @@ public class MessageViewBase : ComponentBase
 
     [Parameter] public IEnumerable<DisplayMessageDto> Messages { get; set; } = new List<DisplayMessageDto>();
 
-    protected override async Task OnInitializedAsync()
-    {
-        var messages = new List<DisplayMessageDto>();
-        for (int i = 0; i < 10; i++)
-        {
-            messages.Add(new DisplayMessageDto(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras cursus justo ante, et sollicitudin arcu mollis sit amet. Sed luctus tempor nisi et dignissim. Etia",
-                "Mr. Test", "test@test.com", DateTime.Now));
-        }
-        
-        Messages = messages;
-    }
+    // protected override async Task OnInitializedAsync()
+    // {
+    //     var messages = new List<DisplayMessageDto>();
+    //     for (int i = 0; i < 10; i++)
+    //     {
+    //         messages.Add(new DisplayMessageDto(
+    //             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras cursus justo ante, et sollicitudin arcu mollis sit amet. Sed luctus tempor nisi et dignissim. Etia",
+    //             "Mr. Test", "test@test.com", DateTime.Now));
+    //     }
+    //     
+    //     Messages = messages;
+    // }
 }
