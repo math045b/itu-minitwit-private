e7f682898e984977165a8d584211bc904051c884
Christoffer grünberg <gryn@itu.dk>
2025-03-13 19:15:47 +0100

    Migration for fix

---
+++ itu-minitwit/Api/Api.DataAccess/Migrations/20250313180902_updated_latest_to_have_key_and_value.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class updated_latest_to_have_key_and_value : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Latest",
+                table: "LatestProcessedSimActions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latest",
+                table: "LatestProcessedSimActions");
+        }
+    }
+}
