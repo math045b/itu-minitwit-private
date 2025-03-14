c8240f2f90f06fb7c0c45f7a26ad3237bab78252
Anthon Castillo Hertzum <anthon.hertzum@gmail.com>
2025-03-14 18:31:05 +0100

    Added MessageRepository.cs

---
+++ itu-minitwit/Web.DataAccess/MessageRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http.Json;
+using Microsoft.Extensions.Configuration;
+using Web.DataAccess.Abstract;
+using Web.Services; 
+using Web.Services.DTO_s;
+
+namespace Web.DataAccess;
+
+public class MessageRepository(HttpClient httpClient) : BaseAPIRepository(httpClient), IMessageRepository
+{
+    private string Endpoint { get; } = "/msgs";
+
+    public Task<IEnumerable<DisplayMessageDto>> GetMessages()
+    {
+        return GetAllAsync<DisplayMessageDto>(Endpoint);
+    }
+    
+    // public Task<bool> PostMessageAsync(DisplayMessageDto message)
+    // {
+    //     return CreateAsync<DisplayMessageDto,>()
+    // }
+}
