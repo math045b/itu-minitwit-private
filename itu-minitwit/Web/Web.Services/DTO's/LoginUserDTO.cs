9a433d9142fe133c6322254b49d3917974f14211
stoftot <gryn@itu.dk>
2025-03-15 19:26:12 +0100

    Created login dto - web

---
+++ itu-minitwit/Web/Web.Services/DTO's/LoginUserDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace Web.Services.DTO_s;
+
+public class LoginUserDTO(string username, string password)
+{
+    public string Username { get; } = username;
+    public string Password { get; } = password;
+}
