1db696e2f1ef75eb44b63566f9346a7534fc367a
stoftot <gryn@itu.dk>
2025-03-15 17:51:59 +0100

    Created logic

--- itu-minitwit/Web/Web/Components/Pages/Login.razor.cs
+++ itu-minitwit/Web/Web/Components/Pages/Login.razor.cs
@@ -1,7 +1,45 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System.Runtime.InteropServices;
+using Microsoft.AspNetCore.Components;
+using Web.Services;
 
 namespace Web.Components.Pages;
 
-public partial class Login : ComponentBase
+public class LoginBase : ComponentBase
 {
+    [Inject] protected NavigationManager NavigationManager { get; set; } = null!;
+    [Inject] private UserState UserState { get; set; } = null!;
+    [SupplyParameterFromForm(FormName = "SignIn")]
+    protected UserModel UserModel { get; set; } = new UserModel();
+    protected string? ErrorMessage { get; set; }
+
+    protected void HandleValidSubmit()
+    {
+        if (!UserModel.IsValid())
+        {
+            ErrorMessage = "Please fill out all fields";
+            return;
+        }
+        
+        if (UserModel.Username == "admin" && UserModel.Password == "password")
+        {
+            UserState.SetUser(UserModel.Username);
+            NavigationManager.NavigateTo("/");
+        }
+        else
+        {
+            ErrorMessage = "Invalid username or password.";
+        }
+    }
 }
+
+public class UserModel
+{
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+
+    public bool IsValid()
+    { 
+        return !(string.IsNullOrEmpty(Username) && string.IsNullOrEmpty(Password));
+    }
+}
+
