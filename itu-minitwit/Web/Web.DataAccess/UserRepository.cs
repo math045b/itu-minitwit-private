177ee44a3664a5210cb9610b2bc2293c1370d918
stoftot <gryn@itu.dk>
2025-03-15 19:26:44 +0100

    Create user repo - web

---
+++ itu-minitwit/Web/Web.DataAccess/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http.Json;
+using Microsoft.Extensions.Configuration;
+using Web.DataAccess.Abstract;
+using Web.Services.DTO_s;
+using Web.Services.Repositories;
+
+namespace Web.DataAccess;
+
+public class UserRepository (HttpClient httpClient, IConfiguration configuration) : 
+    BaseAPIRepository(httpClient, configuration), IUserRepository
+{
+    private string Endpoint { get; } = "login";
+    public async Task<bool> Login(LoginUserDTO dto)
+    {
+        var response = await HttpClient.PostAsJsonAsync($"{ApiBaseUrl}/{Endpoint}", dto);
+        response.EnsureSuccessStatusCode();
+        return (await response.Content.ReadFromJsonAsync<bool>())!;
+    }
+}
