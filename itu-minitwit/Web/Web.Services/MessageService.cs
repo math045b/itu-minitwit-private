b5c0c8944c463d454f91a2b0a289cbde07fa795f
stoftot <gryn@itu.dk>
2025-03-16 08:42:02 +0100

    Created service methods

--- itu-minitwit/Web/Web.Services/MessageService.cs
+++ itu-minitwit/Web/Web.Services/MessageService.cs
@@ -5,6 +5,8 @@ namespace Web.Services;
 public interface IMessageService
 {
     public Task<IEnumerable<DisplayMessageDto>> GetMessages();
+    public Task<IEnumerable<DisplayMessageDto>> GetUsersMessages(GetUsersMessageDTO dto);
+    public Task<IEnumerable<DisplayMessageDto>> GetUserAndFollowsMessages(GetUsersMessageDTO dto);
     // public Task<DisplayMessageDto> CreateMessages(CreateClassroomDto dto);
 }
 
@@ -14,6 +16,16 @@ public class MessageService(IMessageRepository messageRepository) : IMessageServ
     {
         return messageRepository.GetMessages();
     }
-    
+
+    public Task<IEnumerable<DisplayMessageDto>> GetUsersMessages(GetUsersMessageDTO dto)
+    {
+        return messageRepository.GetUsersMessages(dto);
+    }
+
+    public Task<IEnumerable<DisplayMessageDto>> GetUserAndFollowsMessages(GetUsersMessageDTO dto)
+    {
+        return messageRepository.GetUserAndFollowsMessages(dto);
+    }
+
     //TODO: Implement future features for creating messages
 }
