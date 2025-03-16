aeaa1d6ddc1373bb21b82eb754a318732a9d8150
stoftot <gryn@itu.dk>
2025-03-16 08:42:52 +0100

    Create GetUsersMessageDTO.cs

---
+++ itu-minitwit/Web/Web.Services/DTO's/GetUsersMessageDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace Web.Services.DTO_s;
+
+public class GetUsersMessageDTO(string username, int numberOfMessages)
+{
+    public string Username { get; } = username;
+    public int NumberOfMessages { get; } = numberOfMessages;
+}
