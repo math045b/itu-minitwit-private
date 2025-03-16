34550e9d894003e74b27df479904911a7a0fb7d1
stoftot <gryn@itu.dk>
2025-03-16 09:15:52 +0100

    Updated display message dto's

--- itu-minitwit/Api/Api.Services/Dto's/MessageDTO's/DisplayMessageDTO.cs
+++ itu-minitwit/Api/Api.Services/Dto's/MessageDTO's/DisplayMessageDTO.cs
@@ -3,6 +3,7 @@ namespace Api.Services.Dto_s.MessageDTO_s;
 public class DisplayMessageDTO
 {
     public required string Username { get; set; }
+    public required string Email { get; set; }
     public string? Text { get; set; }
     public int? PubDate { get; set; }
     
@@ -11,6 +12,7 @@ public class DisplayMessageDTO
         return "DisplayMessageDTO{" +
                $"Username='{Username}'" +
                $", Text='{Text}'" +
+               $", Email='{Email}'" +
                $", PubDate={PubDate}" +
                '}';
     }
