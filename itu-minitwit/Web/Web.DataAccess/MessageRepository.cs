87c69d8e23b5b93c5d8fe8e0a36ca9cb16e1ca54
stoftot <gryn@itu.dk>
2025-03-16 09:15:09 +0100

    Created reposetory method

--- itu-minitwit/Web/Web.DataAccess/MessageRepository.cs
+++ itu-minitwit/Web/Web.DataAccess/MessageRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http.Json;
+using System.Web;
 using Microsoft.Extensions.Configuration;
 using Web.DataAccess.Abstract;
 using Web.Services; 
@@ -14,6 +15,32 @@ public class MessageRepository(HttpClient httpClient, IConfiguration configurati
     public Task<IEnumerable<DisplayMessageDto>> GetMessages()
     {
         return GetAllAsync<DisplayMessageDto>(Endpoint);
+    }
+
+    
+    public async Task<IEnumerable<DisplayMessageDto>> GetUsersMessages(GetUsersMessageDTO dto)
+    {
+        var endpoint = $"{ApiBaseUrl}{Endpoint}/{dto.Username}";
+        return await GetMessagesAsync(endpoint, dto.NumberOfMessages);
+    }
+
+    public async Task<IEnumerable<DisplayMessageDto>> GetUserAndFollowsMessages(GetUsersMessageDTO dto)
+    {
+        var endpoint = $"{ApiBaseUrl}{Endpoint}/fllws/{dto.Username}";
+        return await GetMessagesAsync(endpoint, dto.NumberOfMessages);
+    }
+
+    private async Task<IEnumerable<DisplayMessageDto>> GetMessagesAsync(string endpoint, int numberOfMessages)
+    {
+        var uriBuilder = new UriBuilder(endpoint);
+        var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+        query["no"] = numberOfMessages.ToString();
+        uriBuilder.Query = query.ToString();
+
+        var response = await HttpClient.GetAsync(uriBuilder.ToString());
+        response.EnsureSuccessStatusCode();
+        var list = await response.Content.ReadFromJsonAsync<IEnumerable<DisplayMessageDto>>();
+        return list ?? [];
     }
     
     // public Task<bool> PostMessageAsync(DisplayMessageDto message)
