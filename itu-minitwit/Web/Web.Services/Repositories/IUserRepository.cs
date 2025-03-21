1a212f2720d9732985f87bb6954bac94307ebbe3
stoftot <gryn@itu.dk>
2025-03-21 17:58:38 +0100

    Created reposetory

--- itu-minitwit/Web/Web.Services/Repositories/IUserRepository.cs
+++ itu-minitwit/Web/Web.Services/Repositories/IUserRepository.cs
@@ -4,5 +4,6 @@ namespace Web.Services.Repositories;
 
 public interface IUserRepository
 {
+    public Task<(bool, string ErrorMessage)> Register(RegisterDto dto);
     public Task<bool> Login(LoginUserDTO dto);
 }
