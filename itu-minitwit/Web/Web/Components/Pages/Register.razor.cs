07bb65ed1a8d02262c45da92e8db812345d26c64
stoftot <gryn@itu.dk>
2025-03-21 17:58:59 +0100

    Implemented UI logic

--- itu-minitwit/Web/Web/Components/Pages/Register.razor.cs
+++ itu-minitwit/Web/Web/Components/Pages/Register.razor.cs
@@ -1,7 +1,56 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Components;
+using Web.Services;
+using Web.Services.DTO_s;
 
 namespace Web.Components.Pages;
 
 public partial class Register : ComponentBase
 {
+    [SupplyParameterFromForm(FormName = "Register")]
+    protected RegisterModel RegisterModel { get; set; } = new();
+
+    protected string ErrorMessage;
+
+    [Inject] protected IUserService UserService { get; set; }
+
+    [Inject] protected UserState UserState { get; set; }
+
+    [Inject] protected NavigationManager Navigation { get; set; }
+
+    protected async Task HandleValidSubmit()
+    {
+        ErrorMessage = string.Empty;
+
+        var registerRequest = new RegisterDto
+        {
+            Username = RegisterModel.Username,
+            Email = RegisterModel.Email,
+            Pwd = RegisterModel.Password
+        };
+        
+        var (Success, Error) = await UserService.Register(registerRequest);
+
+        if (Success)
+        {
+            UserState.SetUser(RegisterModel.Username);
+            Navigation.NavigateTo("/");
+        }
+        else
+        {
+            ErrorMessage = Error;
+        }
+    }
+}
+
+public class RegisterModel
+{
+    [Required] public string Username { get; set; }
+
+    [Required, EmailAddress] public string Email { get; set; }
+
+    [Required, MinLength(1)] public string Password { get; set; }
+
+    [Required, Compare("Password", ErrorMessage = "Passwords do not match")]
+    public string ConfirmPassword { get; set; }
 }
